f6e944b73356d2b870bc7e32b721fc535a918d8a
Dillon Sumner <56370697+DillonSumnerEpicGamer@users.noreply.github.com>
2020-12-02 14:37:18 -0800

    Lesson 2.3 - Random Animal Stampede Complete

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+
+    private float xSpawnRange = 20.0f;
+    private float zSpawnPoint = 20.0f;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int animalIndex = Random.Range(0,animalPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-xSpawnRange, xSpawnRange), 0, zSpawnPoint);
+
+            Instantiate(animalPrefabs[animalIndex],spawnPos,animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
