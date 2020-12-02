325f8206e7a297ce5636d1acf875a2256e6716af
Dillon Sumner <56370697+DillonSumnerEpicGamer@users.noreply.github.com>
2020-12-02 15:23:11 -0800

    Challenge 2 Kinda Complete - Couldn't do the last bonus

---
+++ Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoundsX : MonoBehaviour
+{
+    private float leftLimit = -35;
+    private float bottomLimit = -5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy
+        if (transform.position.x < leftLimit || transform.position.y < bottomLimit)
+        {
+            Destroy(gameObject);
+        } 
+    }
+}
